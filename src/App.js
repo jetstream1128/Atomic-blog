c8a9af434f148b4c5d607328e33e348220ea2f8c
jetstream1128 <bogdanalexeev2007@live.com>
2024-02-24 16:05:56 -0700

    add custom hook usePosts

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useContext, useEffect, useState } from "react";
 import { faker } from "@faker-js/faker";
-import { PostProvider, PostContext } from "./PostContext";
+import { PostProvider, usePosts } from "./PostContext";
 
 function createRandomPost() {
 	return {
@@ -42,7 +42,7 @@ function App() {
 
 function Header() {
 	//3) Consuming context value
-	const { onClearPosts } = useContext(PostContext);
+	const { onClearPosts } = usePosts();
 
 	return (
 		<header>
@@ -59,7 +59,7 @@ function Header() {
 }
 
 function SearchPosts() {
-	const { searchQuery, setSearchQuery } = useContext(PostContext);
+	const { searchQuery, setSearchQuery } = usePosts();
 	return (
 		<input
 			value={searchQuery}
@@ -70,7 +70,7 @@ function SearchPosts() {
 }
 
 function Results() {
-	const { posts } = useContext(PostContext);
+	const { posts } = usePosts();
 	return <p>🚀 {posts.length} atomic posts found</p>;
 }
 
@@ -92,7 +92,7 @@ function Posts() {
 }
 
 function FormAddPost() {
-	const { onAddPost } = useContext(PostContext);
+	const { onAddPost } = usePosts();
 
 	const [title, setTitle] = useState("");
 	const [body, setBody] = useState("");
@@ -123,7 +123,7 @@ function FormAddPost() {
 }
 
 function List() {
-	const { posts } = useContext(PostContext);
+	const { posts } = usePosts();
 	return (
 		<ul>
 			{posts.map((post, i) => (
@@ -138,7 +138,7 @@ function List() {
 
 function Archive() {
 	// Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
-	const { onAddPost } = useContext(PostContext);
+	const { onAddPost } = usePosts();
 
 	const [posts] = useState(() =>
 		// 💥 WARNING: This might make your computer slow! Try a smaller `length` first
